c54d08e3e64f4bde74896739db0c49b780221a7b
SergeySidorchuk <serg_ss@ukr.net>
2022-05-07 00:20:48 +0300

    added media news section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,12 @@
 $(function () {
-  $(".slider__inner").slick({
-    nextArrow: '<button type="button" class="slick-btn slick-next">1</button>',
-    prevArrow: '<button type="button" class="slick-btn slick-prev">2</button>',
+  $('.slider__inner, .news__slider-inner').slick({
+    nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+    prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+    infinite: false,
+  });
+
+  $('select').styler();
+  $('.header__btn-menu').on('click', function () {
+    $('.menu ul').slideToggle();
   });
 });
